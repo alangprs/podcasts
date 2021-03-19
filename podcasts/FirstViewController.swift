c8d688a79be40ffae449d586abe0c282ef64d244
will <alangprs1@gmail.com>
2021-03-19 13:51:53 +0800

    畫面ＵＩ完成

--- podcasts/FirstViewController.swift
+++ podcasts/FirstViewController.swift
@@ -1,14 +1,15 @@
 
-
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    
 
 }
-
